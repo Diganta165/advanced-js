0769a08fcf94a49a8bc2746639d9258559c1bfa2
Diganta165 <mdsrdiganta@gmail.com>
2021-02-02 12:34:44 +0600

    double equal vs triple equal

--- double-vs-triple-equal.js
+++ double-vs-triple-equal.js
@@ -1,8 +1,17 @@
 const first = 2;
 const second = "2";
+const third = 2;
+
 if(first === second){
     comsole.log("condition is true");
 }
 else{
     console.log("condition is false");
+}
+
+if(first == third ){
+    console.log("True");
+}
+else{
+    console.log("False");
 }
