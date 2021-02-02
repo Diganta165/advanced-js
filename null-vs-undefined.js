dff90a68b681092ee428872c3b848a764e50ad92
Diganta165 <mdsrdiganta@gmail.com>
2021-02-02 12:18:39 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(13);
+console.log(result);
+
+const premik ={name: "smart dude ", phone : 12156}
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
